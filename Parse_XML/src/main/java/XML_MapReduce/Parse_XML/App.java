ba8bc5b8f3a917b22fb3e86773c1079de92edf2b
Kishore Rasuri <k.rasuri@gmail.com>
2017-09-08 23:03:27 -0400

    Uploaded the data files

---
+++ Parse_XML/src/main/java/XML_MapReduce/Parse_XML/App.java
@@ -0,0 +1,13 @@
+package XML_MapReduce.Parse_XML;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
