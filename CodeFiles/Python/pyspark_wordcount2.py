55074800b539461d7be82d01dfd14a1f419f9c77
bds01 <k.rasuri@gmail.com>
2020-02-13 14:04:26 -0500

    Create pyspark_wordcount2.py

---
+++ CodeFiles/Python/pyspark_wordcount2.py
@@ -0,0 +1,23 @@
+# Second example of wordcount to print the results to console
+
+import sys
+ 
+from pyspark import SparkContext, SparkConf
+ 
+if __name__ == "__main__":
+	
+	# create Spark context with necessary configuration
+	sc = SparkContext("local","PySpark Word Count Exmaple")
+	
+	# read data from text file and split each line into words
+	words = sc.textFile("hdfs://sandbox-hdp.hortonworks.com:8020/user/spark/contents.txt").flatMap(lambda line: line.split(" "))
+	
+	# count the occurrence of each word
+	wordCounts = words.map(lambda word: (word, 1)).reduceByKey(lambda a,b:a +b)
+	
+	#Collect and print the output
+	output = wordCounts.collect()
+	
+	for (word, count) in output: print("%s: %i" % (word, count))
+
+  spark.stop()
